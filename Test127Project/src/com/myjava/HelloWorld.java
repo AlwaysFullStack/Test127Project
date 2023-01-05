5fc816ecc5553f9bfbff1f8c219ac11b82edbb66
Gulshan <Gulshan@GULSHAN-JAHAN>
2023-01-04 22:15:54 -0500

    One line added

--- Test127Project/src/com/myjava/HelloWorld.java
+++ Test127Project/src/com/myjava/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("This is a hello World Project"+" first ammendment");
 		System.out.println("Added in browser" + " modified in eclipse");
+		System.out.println("Is it still connected.");
 
 	}
 
